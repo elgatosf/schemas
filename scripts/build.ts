da7e5245c69e179279d4c72107015d0e06901f84
Richard Herman <geekyeggo@users.noreply.github.com>
2024-02-12 17:54:33 +0000

    feat: add $id to schemas to allow versioning

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,18 +1,10 @@
-/* eslint-disable no-useless-escape */
 import type { JSONSchema7 } from "json-schema";
 import { existsSync, mkdirSync, writeFileSync } from "node:fs";
 import { join } from "node:path";
 import { createGenerator } from "ts-json-schema-generator";
+import pkg from "../package.json";
 import { customKeywordTransformer } from "./transformers/custom-keywords";
 import { versionManifests } from "./transformers/version-manifests";
-
-// Create a generator so we're able to produce multiple schemas.
-const generator = createGenerator({
-	extraTags: ["errorMessage", "imageDimensions", "filePath"],
-	path: join(__dirname, "../src/streamdeck/plugins/index.ts"),
-	skipTypeCheck: true,
-	tsconfig: join(__dirname, "../tsconfig.json")
-});
 
 // Prepare the output directory.
 const outputDir = join(__dirname, "../streamdeck/plugins");
@@ -20,22 +12,28 @@ if (!existsSync(outputDir)) {
 	mkdirSync(outputDir, { recursive: true });
 }
 
-generateAndWriteSchema("Manifest", versionManifests);
-generateAndWriteSchema("Layout");
+generateAndWriteSchema("Manifest", [customKeywordTransformer, versionManifests]);
+generateAndWriteSchema("Layout", [customKeywordTransformer]);
 
 /**
  * Generates the JSON schema for the specified TypeScript `type`, and writes it locally to `{type}.json`.
  * @param type TypeScript type whose schema should be generated.
- * @param transform Optional function used to transform the schema.
+ * @param transformers Optional function used to transform the schema.
  */
-function generateAndWriteSchema(type: string, transform?: (schema: JSONSchema7) => void): void {
-	const schema = generator.createSchema(type);
-	if (transform) {
-		transform(schema);
-	}
+function generateAndWriteSchema(type: string, transformers?: ((schema: JSONSchema7) => void)[]): void {
+	// Build the generator.
+	const path = join(__dirname, "../src/streamdeck/plugins/index.ts");
+	const generator = createGenerator({
+		extraTags: ["errorMessage", "imageDimensions", "filePath"],
+		path,
+		skipTypeCheck: true,
+		schemaId: `${pkg.name}/streamdeck/plugins/${type.toLowerCase()}@${pkg.version}`,
+		tsconfig: join(__dirname, "../tsconfig.json")
+	});
 
-	// Apply the custom keyword transformer to all schemas
-	customKeywordTransformer(schema);
+	// Generate the schema, and apply the transformers.
+	const schema = generator.createSchema(type);
+	transformers?.forEach((t) => t(schema));
 
 	// Determine the output path, and serialize the schema.
 	const outputPath = join(outputDir, `${type.toLowerCase()}.json`);
