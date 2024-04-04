19adf1a34e14a82ddc3f9f3f64e95083c9155b08
Richard Herman <geekyeggo@users.noreply.github.com>
2024-04-04 18:31:29 +0100

    refactor: update range to be an exported type

--- src/streamdeck/plugins/index.ts
+++ src/streamdeck/plugins/index.ts
@@ -1,7 +1,7 @@
 import { type Layout as LayoutJsonSchema } from "./layout";
 
 export { DeviceType } from "./device-type";
-export { BarSubType, type Bar, type GBar, type Pixmap, type Text } from "./layout";
+export * from "./layout";
 export * from "./manifest/latest";
 
 /**
