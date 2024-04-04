f734df60fca49ef2bc45e0f998fd58ff89396f3c
Richard Herman <geekyeggo@users.noreply.github.com>
2024-04-04 14:38:26 +0100

    fix: export all objects from the manifest

--- src/streamdeck/plugins/index.ts
+++ src/streamdeck/plugins/index.ts
@@ -2,7 +2,7 @@ import { type Layout as LayoutJsonSchema } from "./layout";
 
 export { DeviceType } from "./device-type";
 export { BarSubType, type Bar, type GBar, type Pixmap, type Text } from "./layout";
-export { type Controller, type Manifest } from "./manifest/latest";
+export * from "./manifest/latest";
 
 /**
  * Defines the structure of a custom layout file.
